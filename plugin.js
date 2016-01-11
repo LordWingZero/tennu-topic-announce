548e6dab3e2819f9387a445bc4afbfa0c059dc9e
Victorio Berra <toryberra@gmail.com>
2016-01-11 08:47:00 -0600

    Use new configDefaults

--- plugin.js
+++ plugin.js
@@ -1,12 +1,13 @@
 const response = 'There is no announcement set for the topic yet!';
 var TennuTopicAnnounce = {
+    configDefaults: {
+        "topic-announce": {
+            "message": "huehuehue"
+        }
+    },
     init: function(client) {
 
         var topicConfig = client.config("topic-announce");
-
-        if (!topicConfig) {
-            throw new Error("tennu-topic-announce: is missing some or all of its configuration.");
-        }
 
         return {
             handlers: {
